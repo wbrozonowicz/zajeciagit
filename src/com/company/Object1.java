db69f411436ef380ebcd4d857228bdd77dc9b777
Wojciech Brozonowicz <wbrozonowicz@o2.pl>
2020-03-01 14:31:32 +0000

    changes from itellij on andronix

--- src/com/company/Object1.java
+++ src/com/company/Object1.java
@@ -2,11 +2,14 @@ package com.company;
 
         public class Object1{
             private String name;
+            private String surname;
 Object1(String name){
 this.name = name;
+this.surname = "Brozonowicz";
 }
 
 public void showName(){
-    System.out.println("Hi my name is "+ this.name);
+    System.out.println("Hi my name is "+ this.name
+    + " " + this.surname);
 }
         }
